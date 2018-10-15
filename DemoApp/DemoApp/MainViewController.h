fb71836249d932ebd7c0dff7571a7e3eb91d405f
Marco Pace <marco.pace@jumbo.com>
2018-10-15 09:59:38 +0200

    Added support for Objective-C

---
+++ DemoApp/DemoApp/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  DemoApp
+//
+//  Created by Marco Pace on 15/10/2018.
+//  Copyright © 2018 Marco Pace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
