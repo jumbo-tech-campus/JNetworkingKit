f73007512d9657e93c0ec3d8c45e207c783e199d
Marco Pace <marco.pace@jumbo.com>
2019-04-30 17:06:39 +0200

    Minor fix

--- DemoApp/DemoApp/Network/Examples/Executors/RequestExecutorExtended.swift
+++ DemoApp/DemoApp/Network/Examples/Executors/RequestExecutorExtended.swift
@@ -1,8 +1,7 @@
 import JNetworkingKit
 
 public class RequestExecutorExtended: RequestExecutor {
-    override
-    public var client: RequestClientType {
+    override public var client: RequestClientType {
         return RequestClientExtended()
     }
 }
